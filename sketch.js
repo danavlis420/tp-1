a9a2419d34c31f86b80d6f6afbe142c77d34e9f4
danavlis420 <silva.adaniel.01@gmail.com>
2023-06-13 20:13:46 -0300

    Segunda actualización del proyecto P5JS

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let pSize = 16;
+let pSize = 8;
 let pixel = 0;
 let pixelfondo = 0;
 let contador = 0;
@@ -42,8 +42,8 @@ function draw() {
   }
   // CAPA DE FONDO
   push();
-  for (let x = 0; x < width; x += 16) {
-    for (let y = 0; y < height; y += 16) {
+  for (let x = 0; x < width; x += pSize*2) {
+    for (let y = 0; y < height; y += pSize*2) {
       noStroke();
       if (random() > 0.5) {
         pixelfondo = c1;
@@ -51,86 +51,22 @@ function draw() {
         pixelfondo = c2;
       }
       fill(pixelfondo);
-      ellipse(x, y, 16, 16);
-      ellipse(x + 6, y, 16, 16);
-      ellipse(x, y + 6, 16, 16);
+      ellipse(x, y, pSize*2);
+      ellipse(x + 6, y, pSize*2);
+      ellipse(x, y + 6, pSize*2);
     }
   }
   pop();
   // CAPA MEDIA
-  push();
-  translate(320, 180);
-  scale(2.3);
-  push();
-  noStroke();
-  fill(c6);
-  rect(-50, -50, 100, 100);
-  translate(-64, -68);
-  stroke(c1);
-  strokeWeight(6);
-  strokeCap(SQUARE);
-  line(50, 50, 80, 50);
-  line(46, 54, 46, 84);
-  line(50, 88, 80, 88);
-  line(84, 54, 84, 84);
-  //
-  strokeWeight(6);
-  line(54, 50, 54, 34);
-  line(76, 50, 76, 34);
-  line(65, 50, 65, 34);
-  //
-  line(54, 88, 54, 104);
-  line(65, 88, 65, 104);
-  line(76, 88, 76, 104);
-  //
-  line(46, 58, 30, 58);
-  line(46, 69, 30, 69);
-  line(46, 80, 30, 80);
-  //
-  line(84, 58, 100, 58);
-  line(84, 69, 100, 69);
-  line(84, 80, 100, 80);
-  pop();
-  pop();
-  // CHIP 2
-  push();
-  translate(200, 350);
-  scale(1.8);
-  push();
-  noStroke();
-  fill(c7);
-  rect(-50, -50, 100, 100);
-  translate(-64, -68);
-  stroke(c1);
-  strokeWeight(6);
-  strokeCap(SQUARE);
-  line(50, 50, 80, 50);
-  line(46, 54, 46, 84);
-  line(50, 88, 80, 88);
-  line(84, 54, 84, 84);
-  //
-  strokeWeight(6);
-  line(54, 50, 54, 34);
-  line(76, 50, 76, 34);
-  line(65, 50, 65, 34);
-  //
-  line(54, 88, 54, 104);
-  line(65, 88, 65, 104);
-  line(76, 88, 76, 104);
-  //
-  line(46, 58, 30, 58);
-  line(46, 69, 30, 69);
-  line(46, 80, 30, 80);
-  //
-  line(84, 58, 100, 58);
-  line(84, 69, 100, 69);
-  line(84, 80, 100, 80);
-  pop();
-  pop();
+  if (mouseIsPressed || keyIsPressed){
+    pSize=16;
+  } else{
+    pSize=8;
+  }
   // CAPA DE PIXELES
   push();
-  for (let x = 0; x < width; x += 8) {
-    for (let y = 0; y < height; y += 8) {
+  for (let x = 0; x < width; x += pSize) {
+    for (let y = 0; y < height; y += pSize) {
       noStroke();
       if (random() > 0.7) {
         pixel = cP;
@@ -138,7 +74,7 @@ function draw() {
         pixel = "rgba(0,0,0,0)";
       }
       fill(pixel);
-      rect(x, y, 8, 8);
+      rect(x, y, pSize, pSize);
     }
   }
   pop();
